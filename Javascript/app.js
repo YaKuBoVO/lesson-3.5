07d36de4b22cd0013bda68b9db54f6359529419c
YaKuBoVO <oyakubov526@gmail.com>
2023-05-19 18:21:12 +0500

    qotyapti

---
+++ Javascript/app.js
@@ -0,0 +1,6 @@
+
+IconMenu = document.querySelector(".icon-menu");
+IconMenu.onclick = function() {
+   navbarList = document.querySelector(".navbar-list");
+   navbarList.classList.toggle("active");
+}
