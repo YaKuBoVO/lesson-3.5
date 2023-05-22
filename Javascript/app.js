70b1282d139707b883081a099155991eab33ad01
YaKuBoVO <oyakubov526@gmail.com>
2023-05-22 14:02:51 +0500

    pashti tayyor

--- Javascript/app.js
+++ Javascript/app.js
@@ -4,3 +4,15 @@ IconMenu.onclick = function() {
    navbarList = document.querySelector(".navbar-list");
    navbarList.classList.toggle("active");
 }
+
+
+let dropdown = document.querySelector('.dropdown');
+   dropdown.onclick = function() {
+      dropdown.classList.toggle('active');
+   }
+
+
+let ModalButton = document.querySelector(".modal-button");
+      ModalButton.onclick = function() {
+         ModalButton.classList.toggle("active");
+      }
